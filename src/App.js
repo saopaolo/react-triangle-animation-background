76d8d83b25711e063511488700850e038a20bff1
Vittorio Aquino <vittorio.mcc@gmail.com>
2022-09-13 12:13:26 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="background">
+        <div class="triangle isosceles-triangle"></div>
+        <div class="triangle right-triangle"></div>
+        <div class="triangle equilateral-triangle"></div>
+        <div class="triangle isosceles-triangle"></div>
+      </div>
+    </div>
+  );
+}
